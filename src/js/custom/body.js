5b7dc413bf13bcb9b69bb16bfe3c2f3775bd3d37
João Paulo Duduch <jpduduch@gmail.com>
2019-11-18 09:42:44 -0300

    New layout

---
+++ src/js/custom/body.js
@@ -0,0 +1,14 @@
+//regular a altura do navegador para não cortar info (bugfix para dispositivos móveis)
+
+$(document).ready(function(){
+	if (detectmob()){
+		
+		var $alturaPagina = $(window).height() - $1rem * 8;
+		
+		$('body').css('height', $alturaPagina + 'px');
+	}
+});
+
+$(window).resize(function() {
+	$('body').css('height', '');
+});
