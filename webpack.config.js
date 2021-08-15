be2631df7e40251d08f88d47f6e90a4232336ae8
João Paulo Duduch <jpduduch@gmail.com>
2021-08-15 19:51:23 -0300

    editorconfig inclusion

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const Webpack = require('webpack')
 
 /* Webpack é um compilador que consegue juntar todos os arquivos src como .sass e .js e gerar os outputs a serem lidos pelo browser */
 
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') /* Puxa todos os estilos css de um js e compila num arquivo css, evitando o problema css in js */
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
-const TerserPlugin = require("terser-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin')
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
@@ -25,118 +25,116 @@ const dirNode = 'node_modules'
 // module exports é o comando que exporta as funções etc geradas aqui para serem usadas por outros arquivos
 
 module.exports = {
-    entry: [
-        path.join(dirJs, 'index.js'),
-        path.join(dirSass, 'screen.scss')
-    ],
+  entry: [
+    path.join(dirJs, 'index.js'),
+    path.join(dirSass, 'screen.scss')
+  ],
 
-    resolve: {
-        modules: [
-            dirJs,
-            dirSass,
-            dirImg,
-            dirNode
-        ]
+  resolve: {
+    modules: [
+      dirJs,
+      dirSass,
+      dirImg,
+      dirNode
+    ]
+  },
+
+  devServer: {
+    before: function (app, server) {
+      server._watch('./bin/**/*.html')
     },
+    contentBase: path.join(__dirname),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
 
-    devServer: {
-        before: function (app, server) {
-            server._watch('./bin/**/*.html')
-        },
-        contentBase: path.join(__dirname),
-        hot: true,
-        port: 3000,
-        host: '0.0.0.0'
-    },
+  plugins: [
+    new Webpack.DefinePlugin({
+      IS_DEVELOPMENT
+    }),
+    new CleanWebpackPlugin(),
 
-    plugins: [
-        new Webpack.DefinePlugin({
-            IS_DEVELOPMENT
-        }),
+    // new Webpack.ProvidePlugin({
+    // // uma forma de importar libraries para seu projeto. só incluir aqui dentro que já era. o lado ruim é que pode começar a deixar seu projeto pesado se vc perder o controle
+    // }),
 
-        // new Webpack.ProvidePlugin({
-        // // uma forma de importar libraries para seu projeto. só incluir aqui dentro que já era. o lado ruim é que pode começar a deixar seu projeto pesado se vc perder o controle
-        // }),
-
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        new ImageMinimizerPlugin({
-            minimizerOptions: {
-                // Lossless optimization with custom option
-                // Feel free to experiment with options for better result for you
-                plugins: [
-                    ['gifsicle', { interlaced: true }],
-                    ['jpegtran', { progressive: true }],
-                    ['optipng', { optimizationLevel: 5 }]
-                ]
-            }
-        })
-    ],
-
-    module: {
-        rules: [
-            { // js
-                test: /\.js$/,
-                use: { loader: 'babel-loader' }
-            },
-
-            { // css
-                test: /\.scss$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader, options: { publicPath: '' } },
-                    { loader: 'css-loader?url=false' },
-                    { loader: 'postcss-loader' },
-                    { loader: 'sass-loader' }
-                ]
-            },
-
-            { // imagens
-                test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
-                loader: 'file-loader',
-                options: {
-                    // outputPath: 'images', //coloca numa subpasta
-                    name(file) {
-                        return '[hash].[ext]'
-                    },
-                },
-            },
-
-            { // minifier
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    {
-                        loader: ImageMinimizerPlugin.loader
-                    }
-                ]
-            },
-
-            { //fonts
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-
-
-            { // webGL
-                test: /\.(glsl|frag|vert)$/,
-                loader: 'glslify-loader',
-                exclude: /node_modules/
-            },
-            { // webGL
-                test: /\.(glsl|frag|vert)$/,
-                loader: 'raw-loader',
-                exclude: /node_modules/
-            }
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }]
         ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(), //minimizar somente em produção
-            new TerserPlugin()
+      }
+    })
+  ],
+
+  module: {
+    rules: [
+      { // js
+        test: /\.js$/,
+        use: { loader: 'babel-loader' }
+      },
+
+      { // css
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader, options: { publicPath: '' } },
+          { loader: 'css-loader?url=false' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' }
         ]
-    }
+      },
+
+      { // imagens
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+        loader: 'file-loader',
+        options: {
+          // outputPath: 'images', //coloca numa subpasta
+          name (file) {
+            return '[hash].[ext]'
+          }
+        }
+      },
+
+      { // minifier
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader
+          }
+        ]
+      },
+
+      { // fonts
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
+
+      { // webGL
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/
+      },
+      { // webGL
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(), // minimizar somente em produção
+      new TerserPlugin()
+    ]
+  }
 }
-
-
